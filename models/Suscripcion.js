a8de2b3a10196bc6ed454cea0b1c351f1ed8a7a9
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2024-05-27 23:46:05 -0500

    subiendo suscripciones actualizadas

--- models/Suscripcion.js
+++ models/Suscripcion.js
@@ -1,37 +1,103 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+// const { DataTypes } = require('sequelize');
+// const sequelize = require('../config/db');
 
-const Suscripcion = sequelize.define('Suscripcion', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
+// const Suscripcion = sequelize.define('Suscripcion', {
+//   id: {
+//     type: DataTypes.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true,
+//   },
+//   tipo_suscripcion: {
+//     type: DataTypes.ENUM('Mensual', 'Trimestral', 'Anual'),
+//     allowNull: false,
+//   },
+//   fecha_inicio: {
+//     type: DataTypes.DATE,
+//     allowNull: false,
+//   },
+//   fecha_fin: {
+//     type: DataTypes.DATE,
+//     allowNull: false,
+//   },
+//   createdAt: {
+//     type: DataTypes.DATE,
+//     allowNull: false,
+//     defaultValue: DataTypes.NOW,
+//   },
+//   updatedAt: {
+//     type: DataTypes.DATE,
+//     allowNull: false,
+//     defaultValue: DataTypes.NOW,
+//   },
+// }, {
+//   tableName: 'Suscripciones',
+//   timestamps: true,
+// });
+
+// module.exports = Suscripcion;
+
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const Suscripcion = sequelize.define(
+  "Suscripcion",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    usuario_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    rol: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    tipo_suscripcion: {
+      type: DataTypes.ENUM("Mensual", "Trimestral", "Anual"),
+      allowNull: false,
+    },
+    fecha_inicio: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    fecha_fin: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    payment_id: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    payment_status: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    payment_amount: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true,
+    },
+    payment_currency: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
   },
-  tipo_suscripcion: {
-    type: DataTypes.ENUM('Mensual', 'Trimestral', 'Anual'),
-    allowNull: false,
-  },
-  fecha_inicio: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  fecha_fin: {
-    type: DataTypes.DATE,
-    allowNull: false,
-  },
-  createdAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW, 
-  },
-  updatedAt: {
-    type: DataTypes.DATE,
-    allowNull: false,
-    defaultValue: DataTypes.NOW,
-  },
-}, {
-  tableName: 'Suscripciones',
-  timestamps: true,
-});
+  {
+    timestamps: true,
+    tableName: "Suscripciones",
+  }
+);
 
 module.exports = Suscripcion;

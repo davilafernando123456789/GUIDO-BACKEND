7e8763e1a1f39df8ed5a3f647c747a3b4c69458d
davilafernando123456789 <jose.davila.b@tecsup.edu.pe>
2024-07-01 15:09:44 -0500

    Subiedno conexion correcta

--- config/db.js
+++ config/db.js
@@ -1,18 +1,20 @@
+// config/db.js
 const { Sequelize } = require('sequelize');
 
 // Configuración de conexión a la base de datos en AWS RDS
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+const sequelize = new Sequelize('mydb', 'admin', '#Fernando12', {
+  host: 'guidodatabase.cpsczlory4uj.us-east-1.rds.amazonaws.com',
+  port: 3306,
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false // Agregar esta línea
     }
   },
   logging: console.log,
 });
+
 
 // Verificar la conexión a la base de datos
 sequelize.authenticate()
@@ -33,44 +35,6 @@ sequelize.sync({ force: false }) // Setea a true solo si necesitas recrear las t
   });
 
 module.exports = sequelize;
-
-// // config/db.js
-// const { Sequelize } = require('sequelize');
-
-// // Configuración de conexión a la base de datos en AWS RDS
-// const sequelize = new Sequelize('mydb', 'admin', '#Fernando12', {
-//   host: 'guidodatabase.cpsczlory4uj.us-east-1.rds.amazonaws.com',
-//   port: 3306,
-//   dialect: 'mysql',
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false // Agregar esta línea
-//     }
-//   },
-//   logging: console.log,
-// });
-
-
-// // Verificar la conexión a la base de datos
-// sequelize.authenticate()
-//   .then(() => {
-//     console.log('Conexión exitosa a la base de datos de AWS RDS.');
-//   })
-//   .catch(err => {
-//     console.error('Error al conectar a la base de datos de AWS RDS:', err);
-//   });
-
-// // Sincronizar modelos con la base de datos
-// sequelize.sync({ force: false }) // Setea a true solo si necesitas recrear las tablas
-//   .then(() => {
-//     console.log('Tablas sincronizadas correctamente');
-//   })
-//   .catch((error) => {
-//     console.error('Error al sincronizar las tablas:', error);
-//   });
-
-// module.exports = sequelize;
 
 
 
